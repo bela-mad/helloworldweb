76c48f056fae13046a7becb3c9cb6e4173bd78cd
bela-mad <isabelaargolo3110@gmail.com>
2022-05-15 23:58:52 -0300

    estudando tratamentos e exceptions

---
+++ aulas/src/tratamentoexceptions/TratamentoDeOutrasExceptions.java
@@ -0,0 +1,10 @@
+package tratamentoexceptions;
+
+public class TratamentoDeOutrasExceptions extends IllegalArgumentException{
+
+	private static final long serialVersionUID = 1L;
+
+	public TratamentoDeOutrasExceptions(String ex) {
+		super(ex);
+	}
+}
