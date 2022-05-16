5759928b45b9f436ccf6554dd843dc870e9bdcab
bela-mad <isabelaargolo3110@gmail.com>
2022-05-15 23:55:05 -0300

    criando TesteDebugs.java

---
+++ aulas/src/debugs/TesteDebugs.java
@@ -0,0 +1,20 @@
+package debugs;
+
+public class TesteDebugs {
+	
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		
+		calc.soma(5,4);
+		System.out.println("Resultado: " + calc.subtraçao(10, 8));
+		
+		Calculadora .multiplicaçao(2, 2);
+		
+		
+		
+		
+		
+		
+	}
+
+}
