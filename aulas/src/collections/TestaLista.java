2233e3269b11b1a8ba0237c84652582eb6e5a385
bela-mad <isabelaargolo3110@gmail.com>
2022-05-20 18:35:58 -0300

    aprendendo sobre collections

---
+++ aulas/src/collections/TestaLista.java
@@ -0,0 +1,67 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaLista {
+
+	public static void main(String[] args) {
+		
+		List<Carro> meusCarros = new ArrayList<>();
+		
+		Carro carro1 = new Carro("Audi X1",2022,"vinho","2.2");
+		meusCarros.add(carro1); //inserir informação
+	
+		meusCarros.add(new Carro("Renegade", 2018,"preto","2.0"));
+		meusCarros.add(new Carro("Renegade", 2018,"preto","2.0"));
+		meusCarros.add(new Carro("Etios Sedan", 2019,"vermelho","1.6"));
+		meusCarros.add(new Carro("Etios Sedan", 2019,"vermelho","1.6"));
+		
+		for (Carro carro : meusCarros) {
+			System.out.println(carro.toString());
+		}
+		
+		System.out.println("=====================================");
+		
+		for(int i = 0; i < meusCarros.size(); i++) {
+			Carro carroTemp = meusCarros.get(i);
+			System.out.println(carroTemp.toString());
+		}
+		
+		System.out.println("=====================================");
+		
+		System.out.println(meusCarros.get(1).toString());
+		
+		System.out.println("=====================================");
+		
+//		meusCarros.remove(2);
+//		
+//		for (Carro carro : meusCarros) {
+//			System.out.println(carro.toString());
+//		}
+		
+		System.out.println("=====================================");
+		
+//		boolean resp = meusCarros.contains(carro1);
+		boolean resp = meusCarros.contains(new Carro("Etios Sedan", 2019,"vermelho","1.6"));
+		System.out.println(resp);
+		
+		System.out.println("=====================================");
+		
+//		sort() => Comparable / coloca em ordem alfabetica, nesse caso, o modelo => Strings
+		Collections.sort(meusCarros);
+		System.out.println(meusCarros.toString());
+		
+		System.out.println("=====================================");
+		
+		meusCarros.sort(new ComparadorDeAnos());
+		System.out.println(meusCarros.toString());
+		
+		System.out.println("=====================================");
+		
+		Collections.shuffle(meusCarros); //coloca em ordem aleatoria
+		System.out.println(meusCarros.toString());
+		
+	}
+}
---
+++ aulas/src/collections/TestaStreams.java
@@ -0,0 +1,73 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TestaStreams {
+
+	public static void main(String[] args) {
+		
+		List<String> estudantes = new ArrayList<>();
+		
+		estudantes.add("Maria");
+		estudantes.add("Jose");
+		estudantes.add("Antonio");
+		estudantes.add("Ruy");
+		estudantes.add("Pedro");
+		estudantes.add("Ana");
+		
+		//imprime todos os dados da lista
+		System.out.println("Lista de Alunos");
+		estudantes.stream().forEach(System.out::println);
+		
+		System.out.println("===============================");
+		
+		//Contador
+		System.out.println("Total de EStudantes da lista: " + estudantes.stream().count());
+		
+		//saber qual o maior nome da lista
+		System.out.println("Maior nome: " + estudantes.stream().max(Comparator.comparingInt(String::length)));
+		
+		//saber qual o menor nome da lista
+		System.out.println("Menor nome: " + estudantes.stream().min(Comparator.comparingInt(String::length)));
+				
+		//saber todos os nomes que contem a letra R
+		System.out.println("Nomes que contem r: " + estudantes.stream()
+			.filter((estudante) -> estudante.toLowerCase().contains("r"))
+			.collect(Collectors.toList())
+		);
+		
+		//exibe os n primeiros valores da collection
+		System.out.println("Os primeiros tres itens" + estudantes.stream()
+			.limit(3)
+			.collect(Collectors.toList())
+		);
+		
+		//retorna um valor booleano de acordo com o criterio da busca (verifica se TODAS ocorrencias/nomes tem o elemento)
+		System.out.println("Possui a letra q no nome? " + estudantes.stream()
+			.allMatch((elemento) -> elemento.contains("q"))
+		);
+		
+		//retorna um valor booleano de acordo com o criterio da busca (se pelo menos um valor possuir o elemento, ele passa true e vale para letras maiusculas e minusculas)
+		System.out.println("Alguem possui a letra a no nome? " + estudantes.stream()
+				.anyMatch((elemento) -> elemento.contains("a"))
+		);
+				
+		//retorna um valor booleano de acordo com o criterio da busca (se ninguem possuir o elemento)
+		System.out.println("Ninguem possui a letra w no nome? " + estudantes.stream()
+				.noneMatch((elemento) -> elemento.contains("w"))
+		);
+		
+		//retorna um valor booleano de acordo com o criterio da busca (se ninguem possuir o elemento)
+				System.out.println("Ninguem possui a letra w no nome? " + estudantes.stream()
+						.noneMatch((elemento) -> elemento.contains("w"))
+				);
+		
+		
+		
+		
+		
+	}
+}
