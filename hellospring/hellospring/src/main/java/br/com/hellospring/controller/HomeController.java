675be516d9a379ee5751fa96d883f9a94e50b009
bela-mad <isabelaargolo3110@gmail.com>
2022-06-10 21:12:08 -0300

    adicionando hellospring

---
+++ hellospring/hellospring/src/main/java/br/com/hellospring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package br.com.hellospring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller possui o mesmo comportamento que a servlet, conectando o cliente ao servidor
+@RestController //abstrai algumas annotations auxiliares para exibição de informação na tela
+public class HomeController {
+
+	@RequestMapping("/") //mapeia o endpoint (identificação de funcionalidade na url) que será usado na aplicação
+	//@ResponseBody - usado em conjunto com @Controller para exibição de informações na tela
+	public String homeMessage() {
+		return "Hello World! Hello meninas!!!";
+	}
+}
